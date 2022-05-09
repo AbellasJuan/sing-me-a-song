54c1e3fecbe18d0be9cd1f258eafd0853a182dca
abellasjuan <juanvictoraa@gmail.com>
2022-05-09 10:37:59 -0300

    test: create recommendation with a invalid body

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -34,6 +34,12 @@ describe("POST /recommendations", () => {
         expect(response.status).toBe(201);
     });
 
+    it('should not create a new recommendation given an invalid body', async () => {
+		const body = {};
+		const response = await supertest(app).post('/recommendations').send(body);
+		expect(response.status).toEqual(422);
+	});
+
     it.todo(" post /:id/upvote Adiciona um ponto à pontuação da recomendação. Não espera nada no corpo.")
 
     it.todo(" post /:id/downvote - Remove um ponto da pontuação da recomendação. Não espera nada no corpo. - Se a pontuação fica abaixo de -5, a recomendação deve ser excluída.")
