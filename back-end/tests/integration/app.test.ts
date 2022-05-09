195c6776c0ee9bbf8737578ca521ce99c2edafe3
abellasjuan <juanvictoraa@gmail.com>
2022-05-09 16:20:34 -0300

    test: create get recommendations with id

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -17,7 +17,7 @@ describe("GET /recommendations", () => {
         
     it("should return all recommendations", async () => {
         const body = createRecommendationBody();
-        await supertest(app).post("/recommendations").send(body);
+        await create(body);
         const response = await supertest(app).get("/recommendations");           
         expect(response.status).toBe(200);
     });
@@ -40,21 +40,20 @@ describe('GET /recommendations/top/:amount', () => {
 	});
 });
 
-    it.todo(" get /:id Pega uma recomendação pelo seu ID. ")
-
-describe('GET /recommendations/random', () => {
-	it('should return a random recommendation', async () => {
+describe('GET /recommendations/:id', () => {
+	it('should answer 200, when have an id ', async () => {
         const body = createRecommendationBody();
         const body2 = createRecommendationBody();
         await createRecommendation(body);
         await createRecommendation(body2);
-		const response = await supertest(app).get('/recommendations/random');
-
+        const id = 1;
+		const response = await supertest(app).get(`/recommendations/${id}`);
 		expect(response.status).toEqual(200);
 	});
 
-    it('should 404 without data', async () => {
-		const response = await supertest(app).get('/recommendations/random');
+    it('should 404 without valid id', async () => {
+        const id = 0;
+		const response = await supertest(app).get(`/recommendations/${id}`);
 		expect(response.status).toEqual(404);
 	});
 });
@@ -62,10 +61,10 @@ describe('GET /recommendations/random', () => {
 describe("POST /recommendations", () => {
     it("should return 201 given a valid body", async () => {
         const body = createRecommendationBody();
-        await create(body);
+        const created = await supertest(app).post("/recommendations").send(body);
         const allRecommendations = await findAll();
-        
-        expect(allRecommendations.length).toEqual(1);
+        expect(created.status).toEqual(201);
+        expect(allRecommendations.length).toBe(1)
     });
 
     it('should not create a new recommendation given an invalid body', async () => {
@@ -114,7 +113,3 @@ describe("POST /recommendations/:id/downvote", () => {
 		expect(response.status).toEqual(200);
 	});  
 });
-
-//  it.todo(" post /:id/upvote Adiciona um ponto à pontuação da recomendação. Não espera nada no corpo.")
-
-    // it.todo(" post /:id/downvote - Remove um ponto da pontuação da recomendação. Não espera nada no corpo. - Se a pontuação fica abaixo de -5, a recomendação deve ser excluída.")
