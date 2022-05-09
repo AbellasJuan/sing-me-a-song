abc5a2c54fff9fc407bda08fcdb461954f2deae7
abellasjuan <juanvictoraa@gmail.com>
2022-05-09 01:24:27 -0300

    test: done /recommendation post and get all

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -1,16 +1,38 @@
 import supertest from "supertest"
 import app from "../../src/app.js"
+import { prisma } from "../../src/database.js";
 
-describe("SingMeASong Integration Tests", () => {
-    describe("GET /recommendations", () => {
-        it("should return all recommendations", async () => {
-            
-            const response = await supertest(app).get("/recommendations");
-            console.log(response.body)
-            
-            expect(response.status).toEqual(200);
+afterAll(() => {
+  prisma.$disconnect();
+});
 
-        })
-    })
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY CASCADE;`;
+});
 
-})
+describe("GET /recommendations", () => {
+    
+    it("should return 201 given a valid body", async () => {
+        const body = {
+            name: "pegadona",
+            youtubeLink: "https://www.youtube.com/watch?v=LvR0PFXffsiI",
+        }
+        
+        const response = await supertest(app).post("/recommendations").send(body);
+        expect(response.status).toBe(201);
+    });
+    
+    it("should return all recommendations", async () => {
+
+        const body = {
+            name: "pegadona",
+            youtubeLink: "https://www.youtube.com/watch?v=LvR0PFXffsiI",
+        }
+
+        await supertest(app).post("/recommendations").send(body);
+
+        const response = await supertest(app).get("/recommendations");   
+        console.log(response.body)         
+        expect(response.status).toBe(200);
+    });
+});
