02934a7bfcd4e3cbcdd8db93b76ad5b60e15baa9
abellasjuan <juanvictoraa@gmail.com>
2022-05-09 10:28:30 -0300

    test: get and post recommendations

---
+++ back-end/tests/factory/recommendationsFactory.ts
@@ -0,0 +1,16 @@
+import { faker } from "@faker-js/faker";
+import { prisma } from "../../src/database";
+import { CreateRecommendationData } from "../../src/services/recommendationsService";
+
+export function createRecommendationBody(){
+  return {
+    name: faker.random.words(6),
+    youtubeLink: "https://www.youtube.com/watch?v=LvR0PFX9siI"
+  };
+};
+
+export async function createRecommendation(data: CreateRecommendationData) {
+  return await prisma.recommendation.create({
+    data,
+  });
+}
--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -1,6 +1,7 @@
 import supertest from "supertest"
 import app from "../../src/app.js"
 import { prisma } from "../../src/database.js";
+import { createRecommendationBody } from "../factory/recommendationsFactory.js";
 
 afterAll(() => {
   prisma.$disconnect();
@@ -11,28 +12,30 @@ beforeEach(async () => {
 });
 
 describe("GET /recommendations", () => {
+        
+    it("should return all recommendations", async () => {
+        const body = createRecommendationBody();
+        await supertest(app).post("/recommendations").send(body);
+        const response = await supertest(app).get("/recommendations");           
+        expect(response.status).toBe(200);
+    });
+
+    it.todo(" get /random")
+    it.todo(" get /top/:amount")
+    it.todo(" get /:id Pega uma recomendação pelo seu ID. ")
+    
+});
+
+describe("POST /recommendations", () => {
     
     it("should return 201 given a valid body", async () => {
-        const body = {
-            name: "pegadona",
-            youtubeLink: "https://www.youtube.com/watch?v=LvR0PFXffsiI",
-        }
-        
+        const body = createRecommendationBody();
         const response = await supertest(app).post("/recommendations").send(body);
         expect(response.status).toBe(201);
     });
+
+    it.todo(" post /:id/upvote Adiciona um ponto à pontuação da recomendação. Não espera nada no corpo.")
+
+    it.todo(" post /:id/downvote - Remove um ponto da pontuação da recomendação. Não espera nada no corpo. - Se a pontuação fica abaixo de -5, a recomendação deve ser excluída.")
     
-    it("should return all recommendations", async () => {
-
-        const body = {
-            name: "pegadona",
-            youtubeLink: "https://www.youtube.com/watch?v=LvR0PFXffsiI",
-        }
-
-        await supertest(app).post("/recommendations").send(body);
-
-        const response = await supertest(app).get("/recommendations");   
-        console.log(response.body)         
-        expect(response.status).toBe(200);
-    });
 });
